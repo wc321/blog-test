115e251956fffe4666004a380d5f581a6fe5f233
wc321 <ggul147258@naver.com>
2026-06-28 02:14:42 +0900

    feat(article): BlogApiController REST API 구현(POST)

---
+++ src/main/java/me/woochan/my_blog/controller/BlogApiController.java
@@ -0,0 +1,27 @@
+package me.woochan.my_blog.controller;
+
+import lombok.RequiredArgsConstructor;
+import me.woochan.my_blog.domain.Article;
+import me.woochan.my_blog.dto.AddArticleRequest;
+import me.woochan.my_blog.service.BlogService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class BlogApiController {
+
+    private final BlogService blogService;
+
+    @PostMapping("/api/articles")
+    public ResponseEntity<Article> addArticle(@RequestBody AddArticleRequest request) {
+        Article savedArticle = blogService.save(request);
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(savedArticle);
+    }
+
+}
