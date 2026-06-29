5402c6bd0018bebb66820336ae90e5bb85d1dec7
wc321 <ggul147258@naver.com>
2026-06-30 00:46:34 +0900

    feat(article): BlogService 조회 비즈니스 로직 구현

--- src/main/java/me/woochan/my_blog/service/BlogService.java
+++ src/main/java/me/woochan/my_blog/service/BlogService.java
@@ -6,6 +6,8 @@ import me.woochan.my_blog.dto.AddArticleRequest;
 import me.woochan.my_blog.repository.BlogRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @Service
 public class BlogService {
@@ -14,5 +16,14 @@ public class BlogService {
 
     public Article save(AddArticleRequest addArticleRequest) {
         return blogRepository.save(addArticleRequest.toEntity());
+    }
+
+    public List<Article> findAll() {
+        return blogRepository.findAll();
+    }
+
+    public Article findById(long id) {
+        return blogRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("not found: " + id));
     }
 }
