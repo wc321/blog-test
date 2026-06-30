1fd696dc7c3af582d196aad7027e5a4b0088f75c
wc321 <ggul147258@naver.com>
2026-06-30 18:43:52 +0900

    feat: 블로그 글 목록 페이지 구현
    - ArticleListViewResponse DTO 추가
    - BlogViewController에 GET /articles 엔드포인트 추가
    - articleList.html Thymeleaf 뷰 작성

---
+++ src/main/java/me/woochan/my_blog/controller/BlogViewController.java
@@ -0,0 +1,29 @@
+package me.woochan.my_blog.controller;
+
+import lombok.RequiredArgsConstructor;
+import me.woochan.my_blog.dto.ArticleListViewResponse;
+import me.woochan.my_blog.service.BlogService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Controller
+public class BlogViewController {
+
+    private final BlogService blogService;
+
+    @GetMapping("/articles")
+    public String getArticles(Model model) {
+        List<ArticleListViewResponse> articles = blogService.findAll().stream()
+                .map(ArticleListViewResponse::new)
+                .toList();
+
+        model.addAttribute("articles", articles);
+
+        return "articleList";
+    }
+}
