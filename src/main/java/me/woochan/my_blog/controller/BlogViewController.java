d28cdf70ac4089a88ab4138d515ce8775dfa6624
wc321 <ggul147258@naver.com>
2026-07-08 18:59:22 +0900

    feat: 블로그 글 생성 및 수정 기능 추가
    - BlogViewController에 newArticle() 메서드 추가 (생성/수정 폼 처리)
    - newArticle.html 작성 (생성, 수정 통합 폼)
    - article.js에 생성(POST), 수정(PUT) fetch 로직 추가
    - article.html, articleList.html에 버튼 추가 및 이벤트 연결

--- src/main/java/me/woochan/my_blog/controller/BlogViewController.java
+++ src/main/java/me/woochan/my_blog/controller/BlogViewController.java
@@ -5,11 +5,13 @@ import me.woochan.my_blog.domain.Article;
 import me.woochan.my_blog.dto.ArticleListViewResponse;
 import me.woochan.my_blog.dto.ArticleViewResponse;
 import me.woochan.my_blog.service.BlogService;
+import org.h2.engine.Mode;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -36,5 +38,17 @@ public class BlogViewController {
         model.addAttribute("article", new ArticleViewResponse(article));
 
         return "article";
+    }
+
+    @GetMapping("/new-article")
+    public String newArticle(@RequestParam(required = false) Long id, Model model) {
+        if (id == null) {
+            model.addAttribute("article", new ArticleViewResponse());
+        } else {
+            Article article = blogService.findById(id);
+            model.addAttribute("article", new ArticleViewResponse(article));
+        }
+
+        return "newArticle";
     }
 }
