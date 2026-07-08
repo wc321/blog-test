ea6ee4a7a24d8a6226b6c025e6575f3a61b92a8a
wc321 <ggul147258@naver.com>
2026-07-08 16:50:35 +0900

    feat: 블로그 글 상세 뷰 구현 + JPA Auditing 설정
    - Article 엔티티에 생성시간(createdAt), 수정시간(updatedAt) 추가
    - @EnableJpaAuditing 설정 추가
    - ArticleViewResponse DTO 추가
    - BlogViewController에 getArticle() 추가
    - article.html 상세 뷰 작성

--- src/main/java/me/woochan/my_blog/controller/BlogViewController.java
+++ src/main/java/me/woochan/my_blog/controller/BlogViewController.java
@@ -1,12 +1,15 @@
 package me.woochan.my_blog.controller;
 
 import lombok.RequiredArgsConstructor;
+import me.woochan.my_blog.domain.Article;
 import me.woochan.my_blog.dto.ArticleListViewResponse;
+import me.woochan.my_blog.dto.ArticleViewResponse;
 import me.woochan.my_blog.service.BlogService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -25,5 +28,13 @@ public class BlogViewController {
         model.addAttribute("articles", articles);
 
         return "articleList";
+    }
+
+    @GetMapping("/articles/{id}")
+    public String getArticle(@PathVariable Long id, Model model) {
+        Article article = blogService.findById(id);
+        model.addAttribute("article", new ArticleViewResponse(article));
+
+        return "article";
     }
 }
