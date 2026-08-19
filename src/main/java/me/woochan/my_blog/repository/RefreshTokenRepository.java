7d63d19bfda4630f091c4e5df5a682c469ed0de8
wc321 <ggul147258@naver.com>
2026-08-19 18:47:21 +0900

    feat: RefreshToken 엔티티 및 JWT 인증 필터 추가
    - RefreshToken 도메인 엔티티 작성
    - RefreshTokenRepository 추가 (userId, refreshToken 조회)
    - TokenAuthenticationFilter 구현 (Bearer 토큰 검증 및 SecurityContext 설정)

---
+++ src/main/java/me/woochan/my_blog/repository/RefreshTokenRepository.java
@@ -0,0 +1,12 @@
+package me.woochan.my_blog.repository;
+
+import me.woochan.my_blog.domain.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByUserId(Long userId);
+    Optional<RefreshToken> findByRefreshToken(String refreshToken);
+}
+
